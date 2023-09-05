0881c78dedc33af3ecadb4de1580461712337cfc
FelipeIvo <142915735+FelipeIvo@users.noreply.github.com>
2023-09-05 10:09:26 -0300

    Update maioresmenoresidade.java

--- maioresmenoresidade.java
+++ maioresmenoresidade.java
@@ -1,18 +1,17 @@
-package bicha3;
 import java.util.Scanner; 
 public class maioresmenoresidade  {
 		public static void main (String []args) {
 			Scanner in = new Scanner (System.in);
 			int i=1, n, adulto=0;
 			while (i <= 10) {
-				System.out.println("Qual a Idade da "+i+ "ª Pessoa");
+				System.out.println("Qual a Idade da "+i+ "Âª Pessoa");
 				n = in.nextInt ();
 					if (n>=18) {
 						adulto++; 
 					}
 				i= i+1;}
 			in.close();
-			System.out.println("Há "+adulto+ " adulto(s)");
+			System.out.println("HÃ¡ "+adulto+ " adulto(s)");
 			}
 	}
 
